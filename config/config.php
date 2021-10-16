6759612bac2449f197bf2a80872fb777f81e8053
fabianomsrc <fabiano.msrc@outlook.com>
2021-10-15 22:47:49 -0300

    Adicionadas constantes para definição de namespace, caminho das views e controladores

--- config/config.php
+++ config/config.php
@@ -1,1 +1,7 @@
 <?php
+
+define('NAMESPACE_CONTROLLER', 'App\\Controllers\\');
+
+define('CAMINHO_CONTROLLER', dirname(__DIR__) . '/app/Controllers/');
+
+define('CAMINHO_VIEW', dirname(__DIR__) . '/views/');
