4c1b241941afff6dd2b3e1fc1d6df49dfac6fe58
fabianomsrc <fabiano.msrc@outlook.com>
2021-10-15 22:50:49 -0300

    Adicionada função que instancia a classe Application

--- app/Suporte/Helpers.php
+++ app/Suporte/Helpers.php
@@ -1,1 +1,16 @@
 <?php
+
+use App\Core\Application;
+
+if (! function_exists('app')) {
+    function app(): Application
+    {
+        static $instance = null;
+
+        if (! $instance) {
+            $instance = new Application();
+        }
+
+        return $instance;
+    }
+}
