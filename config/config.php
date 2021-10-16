8aad6165652e3ad32accf51496bc513b004366b8
Fabiano Silva <fabiano.msrc@outlook.com>
2021-10-16 08:14:08 -0300

    Adicionado constantes para credenciais do banco de dados

--- config/config.php
+++ config/config.php
@@ -1,5 +1,11 @@
 <?php
 
+define('MYSQL_DSN', 'mysql:host=127.0.0.1;dbport=3306;dbname=gymweb');
+
+define('DB_USER', 'root');
+
+define('DB_PASS', '');
+
 define('NAMESPACE_CONTROLLER', 'App\\Controllers\\');
 
 define('CAMINHO_CONTROLLER', dirname(__DIR__) . '/app/Controllers/');
