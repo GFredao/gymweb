21340b2896b98446238970577b0d26d7629c12c6
Fabiano Silva <fabiano.msrc@outlook.com>
2021-10-16 08:28:13 -0300

    Adicionado método mágico responsável por conceder acesso a uma propriedade inacessível

--- app/Core/Application.php
+++ app/Core/Application.php
@@ -4,17 +4,33 @@ declare(strict_types=1);
 
 namespace App\Core;
 
+use App\Banco\Banco;
+use App\Suporte\Message;
+
 class Application
 {
+    protected Banco $banco;
+
     protected Core $core;
+
+    protected Message $mensagem;
 
     public function __construct()
     {
+        $this->banco = new Banco();
         $this->core = new Core();
+        $this->mensagem = new Message();
     }
 
     public function run()
     {
         $this->core->resolve();
+    }
+
+    public function __get($name)
+    {
+        if (property_exists($this, $name)) {
+            return $this->$name;
+        }
     }
 }
