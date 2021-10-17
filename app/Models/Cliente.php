0931d339a88fdb0dd12c6e5285ee08f41e5a55a5
Fabiano Silva <fabiano.msrc@outlook.com>
2021-10-17 16:32:23 -0300

    Adicionado método construtor da classe

--- app/Models/Cliente.php
+++ app/Models/Cliente.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 
 namespace App\Models;
 
-use App\Core\Model;
-
 class Cliente extends Model
 {
-
+    public function __construct()
+    {
+        parent::__construct('pessoas', ['id'], ['nome', 'sobrenome', 'email', 'senha']);
+    }
 }
