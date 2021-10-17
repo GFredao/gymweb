cb54714755f4ffaa964bf195b44b95c94578800d
Fabiano Silva <fabiano.msrc@outlook.com>
2021-10-17 09:53:42 -0300

    Adição dos métodos de criação e atualização

--- app/Banco/Contracts/DatabaseManagerInterface.php
+++ app/Banco/Contracts/DatabaseManagerInterface.php
@@ -13,6 +13,10 @@ interface DatabaseManagerInterface
     public function query(string $query, array $valores = []): ?array;
 
     public function leitura(?string $termos = null, ?string $parametros = null, string $colunas = '*'): ?\PDOStatement;
-    
+
+    public function criar(array $dados): ?int;
+
+    public function atualizar(array $dados, string $termos, string $parametros): ?int;
+
     public function excluir(string $termos, ?string $parametros = null): bool;
 }
