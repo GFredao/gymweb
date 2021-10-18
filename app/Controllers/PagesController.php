ca00c0a27e31cf7a5aea61287068e69c7667ab46
Fabiano Silva <fabiano.msrc@outlook.com>
2021-10-17 23:34:29 -0300

    Controlador responsável por gerenciar páginas home, contato etc

--- app/Controllers/PagesController.php
+++ app/Controllers/PagesController.php
@@ -4,39 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
-use App\Core\Controller;
-use App\Views\View;
-
 class PagesController extends Controller
 {
-    public function index()
-    {
-        echo View::make('pages/index');
-    }
 
-    public function home()
-    {
-        echo View::make('pages/home');
-    }
-
-    public function sobre()
-    {
-        echo View::make('pages/sobre');
-    }
-
-    public function contato()
-    {
-        $informacoes = [
-            'nome' => 'John Doe',
-            'email' => 'john.doe@email.com',
-            'fone' => '(00) 0000-0000'
-        ];
-
-        echo View::make('pages/contato', $informacoes);
-    }
-
-    public function parceria()
-    {
-        echo View::make('pages/parcerias');
-    }
 }
