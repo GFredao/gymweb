5143f5038022249e9cf853f0bb5a15b13fdeb0b2
Fabiano Silva <fabiano.msrc@outlook.com>
2021-10-17 23:33:50 -0300

    Controlador responsável por gerenciar dados do usuario cliente

--- app/Controllers/ClienteController.php
+++ app/Controllers/ClienteController.php
@@ -4,28 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
-use App\Core\Controller;
-use App\Views\View;
-
 class ClienteController extends Controller
 {
-    public function index()
-    {
-        echo View::make('cliente/index');
-    }
 
-    public function cadastro()
-    {
-        echo View::make('cliente/cadastro');
-    }
-
-    public function login()
-    {
-        echo View::make('cliente/login');
-    }
-
-    public function logoff()
-    {
-        echo View::make('cliente/logoff');
-    }
 }
