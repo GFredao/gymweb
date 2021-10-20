6b0246b29c75c25c6818f982b6a566048a52d4a1
fabianomsrc <fabiano.msrc@outlook.com>
2021-10-20 12:36:49 -0300

    Adicionado controlador responsável pelo login e registro dos usuários

--- app/Controllers/AccountController.php
+++ app/Controllers/AccountController.php
@@ -4,7 +4,17 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
+use App\Views\View;
+
 class AccountController extends Controller
 {
+    public function index()
+    {
+        echo View::make('signup');
+    }
 
+    public function signin()
+    {
+        echo View::make('signin');
+    }
 }
