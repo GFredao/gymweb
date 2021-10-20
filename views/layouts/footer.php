1a3b9e8c976270fecf4f85868d25c9105e17ba23
fabianomsrc <fabiano.msrc@outlook.com>
2021-10-20 08:37:27 -0300

    Adicionado um espaço entre a constante e o fechamento da tag

--- views/layouts/footer.php
+++ views/layouts/footer.php
@@ -1,6 +1,6 @@
     <!-- Bootstrap -->
-    <script src="<?= URLBASE?>/public/assets/js/core/jquery-3.5.1.slim.min.js"></script>
-    <script src="<?= URLBASE?>/public/assets/js/core/popper.min.js"></script>
-    <script src="<?= URLBASE?>/public/assets/js/core/bootstrap.min.js"></script>
+    <script src="<?= URLBASE ?>/public/assets/js/core/jquery-3.5.1.slim.min.js"></script>
+    <script src="<?= URLBASE ?>/public/assets/js/core/popper.min.js"></script>
+    <script src="<?= URLBASE ?>/public/assets/js/core/bootstrap.min.js"></script>
 </body>
 </html>
