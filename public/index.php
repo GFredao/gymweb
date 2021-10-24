5a8e97c224707e891bf9c95e9f0dacb8f633c528
Fabiano Silva <fabiano.msrc@outlook.com>
2021-10-23 23:54:33 -0300

    Remoção de código desnecessário

--- public/index.php
+++ public/index.php
@@ -5,9 +5,3 @@ require __DIR__ . '/../config/config.php';
 require __DIR__ . '/../app/Suporte/Helpers.php';
 
 app()->run();
-
-$model = new App\Models\Cliente();
-
-$cliente = $model->where('id = :id', 'id = 1')->fetchAll();
-
-var_dump($cliente);
