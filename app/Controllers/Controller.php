b3a06369bf30866385a8f5c17c96df834be0ecb6
Fabiano Silva <fabiano.msrc@outlook.com>
2021-10-23 23:52:17 -0300

    Instanciação de objetos

--- app/Controllers/Controller.php
+++ app/Controllers/Controller.php
@@ -4,7 +4,30 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
+use App\Models\Admin;
+use App\Models\Cliente;
+use App\Models\Dieta;
+use App\Models\Objetivo;
+use App\Models\Treino;
+
 abstract class Controller
 {
+    protected Admin $admin;
 
+    protected Cliente $cliente;
+
+    protected Dieta $dieta;
+
+    protected Objetivo $objetivo;
+    
+    protected Treino $treino;
+
+    public function __construct()
+    {
+        $this->admin = new Admin();
+        $this->cliente = new Cliente();
+        $this->dieta = new Dieta();
+        $this->objetivo = new Objetivo();
+        $this->treino = new Treino();
+    }
 }
