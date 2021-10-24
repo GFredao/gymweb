e897e40de69b79b83eb4b56d9dbeb0a2551470f5
Fabiano Silva <fabiano.msrc@outlook.com>
2021-10-23 23:27:27 -0300

    Adicionado tipagem de retorno

--- app/Controllers/PagesController.php
+++ app/Controllers/PagesController.php
@@ -4,7 +4,12 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
+use App\Views\View;
+
 class PagesController extends Controller
 {
-
+    public function index(): void
+    {
+        echo View::make('pages/home');
+    }
 }
