f9832d3ca50dd3c40f5a61caf446241ba1989378
Fabiano Silva <fabiano.msrc@outlook.com>
2021-10-23 23:53:24 -0300

    Adição de mais duas constantes para definição do header e do footer

--- config/config.php
+++ config/config.php
@@ -15,3 +15,7 @@ define('NAMESPACE_CONTROLLER', 'App\\Controllers\\');
 define('CAMINHO_CONTROLLER', dirname(__DIR__) . '/app/Controllers/');
 
 define('CAMINHO_VIEW', dirname(__DIR__) . '/views/');
+
+define('HTML_HEADER', dirname(__DIR__) . '/views/layouts/header.php');
+
+define('HTML_FOOTER', dirname(__DIR__) . '/views/layouts/footer.php');
